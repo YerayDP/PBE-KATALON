c52bf35dd557bc1537a5339ce24a91e9a2241495
Yeray Dominguez Pavon <y.dominguez@wata.es>
2022-08-10 08:35:40 +0200

    COMMIT PBE

---
+++ Object Repository/Page_Porsche/tbody_Name                                 _a8830f.rs
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tbody_Name                                 _a8830f</name>
+   <tag></tag>
+   <elementGuidId>3a442b86-eed3-4c91-a545-2ed8e325c1b3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='table_login_with_message_down']/tbody</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>tbody</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>tbody</value>
+      <webElementGuid>f3a008cf-5938-4d9a-ac17-e6f739e871da</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        Name:
+                    
+                    
+                        
+                    
+                    
+                        Password:
+                    
+                    
+                        
+                    
+                    
+                        
+                            
+                                                            
+                                    Forgot
+                                        your password?
+                                
+                                                        
+                            
+                                Login
+                            
+                        
+                    
+                </value>
+      <webElementGuid>9e23e9b4-93d2-4bfa-bd6e-573985e85148</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;table_login_with_message_down&quot;)/tbody[1]</value>
+      <webElementGuid>a8525ebc-acb4-4ac8-8d79-972b06a41706</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//table[@id='table_login_with_message_down']/tbody</value>
+      <webElementGuid>27af3c02-cb22-4e3f-9119-f733753c2797</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Create. Future. Performance.'])[1]/following::tbody[1]</value>
+      <webElementGuid>b7bf4daa-78bb-4785-865a-1c3eca93d137</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tbody</value>
+      <webElementGuid>f423122e-ca8a-40a9-a896-4dbe19c0597e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//tbody[(text() = '
+                        Name:
+                    
+                    
+                        
+                    
+                    
+                        Password:
+                    
+                    
+                        
+                    
+                    
+                        
+                            
+                                                            
+                                    Forgot
+                                        your password?
+                                
+                                                        
+                            
+                                Login
+                            
+                        
+                    
+                ' or . = '
+                        Name:
+                    
+                    
+                        
+                    
+                    
+                        Password:
+                    
+                    
+                        
+                    
+                    
+                        
+                            
+                                                            
+                                    Forgot
+                                        your password?
+                                
+                                                        
+                            
+                                Login
+                            
+                        
+                    
+                ')]</value>
+      <webElementGuid>42538bda-700a-474f-9f78-50df69a2caf4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
